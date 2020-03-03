7a5c0f74d98040aa7c2cef5ae6ec0c2af0777f3c
Diego Villamil <diego.racedb@gmail.com>
2020-03-03 23:42:58 +0800

    Fixed bug where success payload would send even though the user was not found

--- auth_server/controller.js
+++ auth_server/controller.js
@@ -12,22 +12,29 @@ exports.userLogin = (req, res) => {
   console.log("User Log In");//Debug line
 	console.log(req.body)
 	let sqlQrYStr = "SELECT * FROM user_account WHERE username=\'"+req.body.username + "\';";
+	//TODO: Add password to querry when the password fieldhas been changed
 	console.log(sqlQrYStr);
 	let result = db.query(sqlQrYStr, (err, result) => {
 		console.log(result);//Debug line
 
 		//Case 1: querry error or user does not exist
-    if(err || result == {}){
+    if(err || !result.length){
+			console.log("Case 1\n");
 			if(err) throw err;
 			res.send({
 	      result: result,
 	      success: true
 	    });
 		}else if(false){
+			console.log("Case 2\n")
 			//TODO: Case 2: Password does not match the username
 		}else{
 			//Case 3: username and password are valid
-			let auth_token = jwt.sign(result.id, 'THIS_IS_A_SECRET')
+			console.log("Case 3\n");
+			let payload = {
+				_id: result._id
+			}
+			let auth_token = jwt.sign(payload, 'THIS_IS_A_SECRET')
 			res.send({
 	      result: result,
 	      success: true,
