0c4bb718c59927c5ac2b97d8bf038829e00d7cb3
Diego Villamil <diego.racedb@gmail.com>
2020-03-04 12:16:09 +0800

    controllers are now stored in a folder; "controllers".

--- auth_server/controller.js
+++ auth_server/controller.js
@@ -1,49 +1,5 @@
 const mysqlCon = require('mysql')
 const jwt = require('jsonwebtoken')
+const usr_cntrllr = require('./controllers/user_controller')
 
-var db = mysqlCon.createConnection({
-	host: "localhost",
-  user: "racedbadmin",
-	password: "letskamown",
-	database: "racedb"
-});
-
-exports.userLogin = (req, res) => {
-  console.log("User Log In");//Debug line
-	console.log(req.body)
-	let sqlQrYStr = "SELECT * FROM user_account WHERE username=\'"+req.body.username + "\';";
-	//TODO: Add password to querry when the password fieldhas been changed
-	console.log(sqlQrYStr);
-	let result = db.query(sqlQrYStr, (err, result) => {
-		console.log(result);//Debug line
-
-		//Case 1: querry error or user does not exist
-    if(err || !result.length){
-			console.log("Case 1, user not found\n");
-			if(err) throw err;
-			res.send({
-	      result: result,
-	      success: false
-	    });
-		}else if(!(req.body.password === result.password)){
-			console.log("Case 2, invalid password\n")
-			res.send({
-	      result: result,
-	      success: false
-	    });
-			//TODO: Do hashed password check here
-		}else{
-			//Case 3: username and password are valid
-			console.log("Case 3, login success\n");
-			let payload = {
-				_id: result._id
-			}
-			let auth_token = jwt.sign(payload, 'THIS_IS_A_SECRET')
-			res.send({
-	      result: result,
-	      success: true,
-				token: auth_token
-	    });
-		}
-  });
-}
+exports.userLogin = usr_cntrllr.userLogin;
