dacf22cb6f9187a968740dd7f55c658f329c5c5d
jaeson Cho <jaeson.racedb@gmail.com>
2020-05-20 02:41:30 +0800

    5th attempt deployment

---
+++ client/src/pages/HomeNav.js
@@ -0,0 +1,115 @@
+import React,{Component, useState, useEffect} from 'react';
+import logoRDB from "./imgs/racedblogo-04-scaled.png";
+export default class HomeNav extends Component{
+  constructor(props){
+    super(props)
+    const url = require('url');
+    const http = require('http');
+    const queryString =window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const loggedInQuery = urlParams.get('loggedIn');
+    const tokenQuery = urlParams.get('token');
+    this.state={
+      loggedIn: props.loggedIn,
+      name: props.name,
+      username: props.username,
+      token:props.token
+    }
+    console.log("NAME IS : "+this.state.name)
+  
+    fetch('http://localhost:3001/token-info/',{
+      headers:{
+        'Authorization': 'Bearer '+this.state.token,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).catch(()=>{
+      console.log("ERRORS???")
+    })
+    .then(res=>{
+      if(res.status===403){
+        this.setState({
+          loggedIn:false
+        })
+        return res.json()
+      }
+      else{return res.json()}
+    })
+    .then((body,err)=>{
+      console.log("WELCOME: "+ body);
+        console.log("Logged In")
+        this.setState({
+          name: body.name,
+        })
+
+    })
+      // fetch('http://localhost:3001/refresh-user/',{
+      //     method:"POST",
+      //     headers:{
+      //       'Content-Type': 'applicaton/json',
+      //     },
+      //     body: JSON.stringify({username: this.state.username})
+      //   })
+      //   .then( res=>{return res.json()})
+      //   .then((body,err)=>{
+      //     this.setState({
+      //       token: body.token
+      //     })       
+      //   })
+    // fetch('http://localhost:3001/check-token')
+  }
+
+
+  render(){
+    const loginUrl = "http://localhost:3000/login";
+    const signupUrl = "http://localhost:3000/signup";
+    const logoutUrl = "http://localhost:3000/"
+    let {name} = this.state;
+    let {loggedIn} = this.state;
+    let {token} = this.state;
+    const homeUrl ="http://localhost:3000/?&loggedIn="+loggedIn+"&token="+token
+    function IsLoggedIn(props){
+      if(loggedIn){
+        return <nav className="navbar navbar-expand-sm navbar-dark" id="homepageNav">
+        <a className="navbar-brand ml-3" href={homeUrl}>
+          <img src={logoRDB} alt="logo" style= {{width:"110px"}}/>
+        </a>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+           <h5> Hi, {name}</h5>
+          </li>
+          <li className="nav-item">
+            <a className="btn btn-outline-light my-2 my-sm-0" href="http://localhost:3000/add-event" role="button">Create A Race</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href={logoutUrl}>Logout</a>
+          </li>
+        </ul>
+      </nav>
+      }else{
+        return <nav className="navbar navbar-expand-sm navbar-dark" id="homepageNav">
+                <a className="navbar-brand ml-3" href={homeUrl}>
+                  <img src={logoRDB} alt="logo" style= {{width:"110px"}}/>
+                </a>
+
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a className="btn btn-outline-light my-2 my-sm-0" href="http://localhost:3000/add-event" role="button">Create A Race</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href={signupUrl}>Sign Up</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href={loginUrl}>Login</a>
+                  </li>
+                </ul>
+              </nav>
+      }
+    }
+    return(
+    <IsLoggedIn/>
+    
+    )
+  }
+}
---
+++ client/src/pages/login-signup.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import Cookies from 'universal-cookie';
+import {Helmet} from "react-helmet";
+import logoRDB from "./imgs/racedblogo-04-scaled.png"
+import HomeNav from "./HomeNav.js";
+
+export default class User extends Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      username: '',
+      password:'',
+      loggedIn:false
+    }
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.login  = this.login.bind(this);
+  }
+  handlePassword(e){
+    this.setState({
+      password: e.target.value
+    })
+  }
+  handleUsername(e){
+    this.setState({
+      username: e.target.value
+    })
+  }
+  login(e) {
+    e.preventDefault()
+    console.log("Login button pressed\n");
+    const credentials = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    fetch('http://localhost:3001/userLogin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    })
+      .then(response => response.json())
+      .then(body => {
+        // expecting { success: false }
+        // OR { success: true, token, username }
+
+        if (!body.success) {
+          alert('Unable to login')
+        }
+
+        else {
+          // save the token as a cookie
+          alert("Login successful!");
+          this.setState({
+            loggedIn:true,
+            token: body.token,
+            rToken: body.refresh_token
+          })
+          const redirectUrl = "http://localhost:3000/?loggedIn=true&token="+body.token
+          window.location.replace(redirectUrl);
+
+
+        }
+      })
+
+  }
+  render() {
+    return (
+	  //<link rel="stylesheet" href="styling.css">
+		/*<div>
+			<nav class="navbar">
+				<a id="face" href="https://www.facebook.com">
+					<h2>face</h2>
+				</a>
+
+
+				<form id="login">
+					<input type="text" id="l-username" placeholder="Email" />
+					<input type="password" id="l-password" placeholder="Password" />
+					<button id="login" onClick={this.login}>Log In</button>
+				</form>
+			</nav>
+
+			<aside id="signup">
+				<h2>Sign Up</h2>
+					<form>
+						<input type="text" id="s-name" placeholder="Name" /> <br/>
+						<input type="text" id="s-email" placeholder="Email" /> <br/>
+						<input type="password" id="s-password" placeholder="Password" /> <br/>
+						<input type="text" id="s-about" placeholder="About" /> <br/>
+						<input type="text" id="s-birthday" placeholder="Birthday" /> <br/>
+
+						<button onClick={this.signup}>Sign Up</button>
+					</form>
+			</aside>
+		</div> */
+
+    <div class="application">
+    <helmet>
+      <meta charset="utf-8"/>
+      {/*--sets width to device size, sets zoom-->*/}
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+      <title>raceDB Homepage</title>
+      <link rel="stylesheet" href="main.css"/>
+
+      {/*// --bootstrap stuff--*/}
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"/>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    </helmet>
+
+    <div class="body">
+    <div class="content">
+      <div class="content-inside">
+
+    <HomeNav/>
+
+    <div class="m-5 border">
+    	<h3 class="mt-3 ml-5">Log In</h3>
+
+    	{/*<!--form-->*/}
+    	<div class="p-3">
+    		<div class="row ml-4">
+          <div class="col-xs ml-2">
+    				<a>Username</a>
+    			</div>
+    			<div class="col-lg-4">
+    				<input type="text" onChange={this.handleUsername} class="form-control"/>
+    			</div>
+    		</div>
+        <div class="row ml-4 mt-4">
+          <div class="col-xs ml-2">
+    				<a>Password</a>
+    			</div>
+    			<div class="col-lg-4">
+    				<input type="password" onChange={this.handlePassword} class="form-control"/>
+          </div>
+        </div>
+        <div class="row mt-3 ml-4">
+          <button type="button" onClick={this.login} class="btn btn-primary">Log In</button>
+        </div>
+      </div>
+    </div>
+    </div></div></div></div>
+
+    )
+  }
+}
