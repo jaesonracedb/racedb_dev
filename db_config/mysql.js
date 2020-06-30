9a58305a9c5fa306e4c22c24057a6f6d56b46e56
root <root@ip-172-31-19-210.us-east-2.compute.internal>
2020-06-30 13:56:32 +0000

    Added RDS hostname

--- db_config/mysql.js
+++ db_config/mysql.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 module.exports = mysql.createConnection({
-  host: "localhost",
-	user: "racebdadmin",
-	password: "letskamown",
+  host: "racedb-2.cqebmdklzbpy.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Race63Db56",
+  port: 3306,
   database: "racedb",
   multipleStatements: true
 });
